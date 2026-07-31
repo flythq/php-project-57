2d85fc09854e2acefe8efa02a45bafb6ac815cfb
Andrey Nikolaenkov <nichexplore@gmail.com>
2026-07-31 12:29:22 +0300

    Сохранение фильтра в пагинации задач через withQueryString

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -33,7 +33,7 @@ class TaskController extends Controller
             )
             ->with(['status', 'createdBy', 'assignedTo', 'labels'])
             ->orderByDesc('id')
-            ->paginate(2)->withQueryString();
+            ->paginate(15)->withQueryString();
 
         $statuses = TaskStatus::orderBy('name')->get();
         $users = User::orderBy('name')->get();
